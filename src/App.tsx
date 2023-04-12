7dfd34519126662071d3c61dad42a8b18d141489
Alex Xavier <alexjr.123@hotmail.com>
2023-04-11 22:05:41 -0300

    Armazena Tarefas no localStorage do Navegador

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { ItemList } from "./components/ItemList";
 import { AddTask } from "./components/AddTask";
-import { v4 } from 'uuid';
+import { v4 } from "uuid";
+import { Button } from "./components/Button";
 
 export interface TaskProps {
   id: string;
@@ -12,42 +13,77 @@ export interface TaskProps {
 }
 
 function App() {
-  const [taskList, setTaskList] = useState<TaskProps[]>([
-    {
-      id: v4(),
-      titulo: "task 1",
-      data: new Date(),
-      status: false,
-    },
-    {
-      id: v4(),
-      titulo: "task 2",
-      data: new Date(),
-      status: true,
-    },
-  ]);
+  const [taskList, setTaskList] = useState<TaskProps[]>(
+    JSON.parse(window.localStorage.getItem("tasklist-db") || "[]")
+  );
 
-  const handleAddTask = (task:TaskProps) => {
-    setTaskList([task,...taskList]);
-  }
+  useEffect(() => {
+    const valueDB = JSON.stringify(taskList);
+    window.localStorage.setItem("tasklist-db", valueDB);
+  }, [taskList]);
+
+  const handleAddTask = (task: TaskProps) => {
+    setTaskList([task, ...taskList]);
+  };
+
+  const getTaskItem = (id: string) => {
+    return taskList.find((taskItem) => {
+      return taskItem.id === id;
+    });
+  };
+
+  const handleDelete = (id: string) => {
+    const task = getTaskItem(id);
+
+    if (!task) return;
+
+    const index = taskList.indexOf(task);
+    taskList.splice(index, 1);
+
+    setTaskList([...taskList]);
+  };
+
+  const handleDone = (id: string) => {
+    const task = getTaskItem(id);
+
+    if (!task) return;
+
+    const index = taskList.indexOf(task);
+
+    taskList[index].status = true;
+
+    setTaskList([...taskList]);
+  };
 
   const sortByDate = () => {
-    setTaskList(estadoAnterior =>{
-      return estadoAnterior.sort((a, b) =>{
-        return a.data.getTime() - b.data.getTime();
-      })
-    })
-  }
-  
+    setTaskList((estadoAnterior) => {
+      const copiaLista = [...estadoAnterior];
+
+      return copiaLista.sort((a, b) => {
+        return new Date(a.data).getTime() - new Date(b.data).getTime();
+      });
+    });
+  };
+
   return (
     <div className="container">
       <h1>Minha agenda</h1>
       <AddTask onAddTask={handleAddTask} />
-      <main className="card">
-        { taskList.map((task,indice) => {
-          return <ItemList task={task} key={task.id} />
-        }) }
-      </main>
+      {taskList.length > 0 && (
+        <main className="card">
+          <Button onClick={sortByDate}>Ordenar por Data</Button>
+          {taskList.map((task, indice) => {
+            return (
+              <ItemList
+                onDelete={handleDelete}
+                onDone={handleDone}
+                task={task}
+                key={task.id}
+              />
+            );
+          })}
+        </main>
+      )}
     </div>
   );
 }
--- src/components/ItemList/index.tsx
+++ src/components/ItemList/index.tsx
@@ -5,23 +5,52 @@ import "./style.css";
 
 interface ItemListProps {
   task: TaskProps;
+  onDelete: (id: string) => void;
+  onDone: (id: string) => void;
 }
 
-export const ItemList: React.FC<ItemListProps> = ({ task:{ titulo, data, status } }) => {
+export const ItemList: React.FC<ItemListProps> = ({
+  task: { titulo, data, status, id },
+  onDelete,
+  onDone,
+}) => {
+  const taskData = new Date(data).getTime();
+  const todayDate = new Date();
+
+  const today = new Date(
+    todayDate.getFullYear(),
+    todayDate.getMonth(),
+    todayDate.getDate()
+  ).getTime();
+
   return (
-    <div className="list-item">
+    <div
+      className="list-item"
+      style={{
+        color:
+         status ? "green" : (taskData < today ? "red" : (taskData == today ? "orange" : "black")),
+      }}
+    >
       <span className="titulo">{titulo}</span>
       <time className="data">
-        {data.toLocaleDateString("pt-br", { dateStyle: "long" })}
+        {new Date(data).toLocaleDateString("pt-br", { dateStyle: "long" })}
       </time>
       <span className="status">{status ? "Concluído" : "A fazer"}</span>
       <div className="acoes">
         {!status && (
-          <Button className="success" title="Concluir Tarefa">
+          <Button
+            className="success"
+            title="Concluir Tarefa"
+            onClick={() => onDone(id)}
+          >
             <FaCheck />
           </Button>
         )}
-        <Button className="danger" title="Excluir Tarefa">
+        <Button
+          className="danger"
+          onClick={() => onDelete(id)}
+          title="Excluir Tarefa"
+        >
           <FaTrashAlt />
         </Button>
       </div>
